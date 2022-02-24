3bf1db1888920da94ce155e16caa722dc71b33fc
Frank <frank_efa1@hitutoracdm.com>
2022-02-24 11:41:20 +0800

    Add comment

--- NQueensProblem/Program.cs
+++ NQueensProblem/Program.cs
@@ -1,12 +1,15 @@
 ﻿Console.WriteLine("- N Queens Problem -");
-
 int n = GetNumber();
 int counter = 0;
+
 for (int i = 0; i < n; i++)
 {
     PutQueen(i, 0, new int[n]);
 }
 
+/// <summary>
+/// 取得 N (使用者輸入)
+/// </summary>
 int GetNumber()
 {
     Console.Write("Please enter N(integer): ");
@@ -22,15 +25,17 @@ int GetNumber()
     }
     return n;
 }
-
+/// <summary>
+/// 放置皇后，並持續試出下一步路
+/// </summary>
 void PutQueen(int x, int y, int[] board)
 {
-    //下棋
+    //放置皇后
     board[y] = x;
 
     if (y == n - 1)
     {
-        //算出解法
+        //輸出解法
         counter++;
         Output(board);
         return;
@@ -43,10 +48,13 @@ void PutQueen(int x, int y, int[] board)
             if (CheckPlace(board, i, y + 1))
                 PutQueen(i, y + 1, board);
         }
+
         return;
     }
 }
-
+/// <summary>
+/// 確認該位置是否可放置皇后
+/// </summary>
 bool CheckPlace(int[] board, int x, int y)
 {
     for (int i = 0; i < y; i++)
@@ -56,7 +64,9 @@ bool CheckPlace(int[] board, int x, int y)
     }
     return true;
 }
-
+/// <summary>
+/// 輸出棋盤
+/// </summary>
 void Output(int[] board)
 {
     Console.WriteLine($"Solution {counter}:\n");
